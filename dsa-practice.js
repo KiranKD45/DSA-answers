39687defe46dd4468b73289963c8b5e345957da4
kiran madiwalar <kiranm81040@gmail.com>
2023-07-01 11:44:11 +0530

    dsa practice

---
+++ dsa-practice.js
@@ -0,0 +1,20 @@
+//dsa
+// Q1 two sum
+
+// var twoSum =function (nums, target) {
+//     for (var i = 0; i<nums.length; i++) {
+//         for (var j = i+1; j<nums.length; j++) {
+//             if (nums[i]+nums[j]==target) {
+//                 console.log([i,j]);
+//             }
+//         }
+//     }
+// };
+// var nums = [ 0, 4, 45, 6, 10, -8 ]
+// var target = 45;
+// twoSum(nums, target);
+
+//Q2 add two numbers
+
+
+
